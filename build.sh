dd81e637dd50847096c12065320e422eec48a3de
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2022-11-07 13:19:08 +0000

    add response

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-WASMEDGE_CMD
+WASMEDGE_CMD=wasmedge
+
+if [ -n "$WASMEDGE_BUILD_DIR" ]; then
+    WASMEDGE_CMD=$WASMEDGE_BUILD_DIR/tools/wasmedge/wasmedge
+fi
+
+check_wasmedge() {
+    if command -v $WASMEDGE_CMD > /dev/null; then
+        local wasmedge_ou
