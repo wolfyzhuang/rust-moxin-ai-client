cd3506f45ba7c19ae10b089cd7bc27726dd255dd
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2022-11-21 08:40:59 +0000

    Manifest

--- moxin-backend/src/store/download_files.rs
+++ moxin-backend/src/store/download_files.rs
@@ -165,3 +165,80 @@ impl DownloadedFile {
 
     pub fn get_by_id(conn: &rusqlite::Connection, id: &str) -> rusqlite::Result<Self> {
         conn.query_row("SELECT * FROM download_files WHERE id = ?1", [id], |row| {
+            Self::from_row(row)
+        })
+    }
+
+    pub fn remove(file_id: &str, conn: &rusqlite::Connection) -> rusqlite::Result<()> {
+        conn.execute(
+            "DELETE FROM download_files WHERE id = ?1",
+            rusqlite::params![file_id],
+        )?;
+        Ok(())
+    }
+}
+
+pub fn create_table_download_files(conn: &rusqlite::Connection) -> rusqlite::Result<()> {
+    conn.execute_batch(
+        "BEGIN;
+        CREATE TABLE IF NOT EXISTS download_files (
+            id TEXT PRIMARY KEY,
+            model_id TEXT NOT NULL,
+            name TEXT NOT NULL,
+            size TEXT NOT NULL,
+            quantization TEXT NOT NULL,
+            prompt_template TEXT DEFAULT '',
+            reverse_prompt TEXT DEFAULT '',
+            downloaded INTEGER DEFAULT 0,
+            file_size UNSIGNED BIG INT DEFAULT 0,
+            download_dir TEXT NOT NULL,
+            downloaded_at TEXT NOT NULL,
+            tags TEXT NOT NULL,
+            featured INTEGER DEFAULT 0,
+            sha256 TEXT NOT NULL DEFAULT ''
+        );
+        CREATE INDEX IF NOT EXISTS index_model_id ON download_files (model_id);
+        CREATE INDEX IF NOT EXISTS index_downloaded ON download_files (downloaded);
+        COMMIT;",
+    )?;
+
+    Ok(())
+}
+
+#[test]
+fn test_sql() {
+    let conn: rusqlite::Connection = rusqlite::Connection::open_in_memory().unwrap();
+    create_table_download_files(&conn).unwrap();
+
+    let mut downloaded_file = DownloadedFile {
+        id: Arc::new("test".to_string()),
+        model_id: "test".to_string(),
+        name: "test".to_string(),
+        size: "test".to_string(),
+        quantization: "test".to_string(),
+        prompt_template: "test".to_string(),
+        reverse_prompt: "test".to_string(),
+        downloaded: false,
+        file_size: 1024,
+        download_dir: "test".to_string(),
+        downloaded_at: Utc::now(),
+        tags: vec!["test".to_string()],
+        featured: false,
+        sha256: Default::default(),
+    };
+
+    downloaded_file.insert_into_db(&conn).unwrap();
+    let files = DownloadedFile::get_all(&conn).unwrap();
+    assert_eq!(files.len(), 1);
+    assert_eq!(files[&downloaded_file.id], downloaded_file);
+
+    let files = DownloadedFile::get_finished(&conn).unwrap();
+    assert_eq!(files.len(), 0);
+
+    downloaded_file.mark_downloads();
+    downloaded_file.update_downloaded(&conn).unwrap();
+
+    let files = DownloadedFile::get_finished(&conn).unwrap();
+    assert_eq!(files.len(), 1);
+    assert_eq!(files[&downloaded_file.id], downloaded_file);
+}
