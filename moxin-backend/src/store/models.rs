3fc013a7ef6e8c3e9e39390688021809b4ea3780
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2022-11-22 15:33:34 +0000

    some code cleaning

--- moxin-backend/src/store/models.rs
+++ moxin-backend/src/store/models.rs
@@ -9,4 +9,10 @@ pub fn create_table_models(conn: &rusqlite::Connection) -> rusqlite::Result<()> 
     conn.execute(
         "CREATE TABLE IF NOT EXISTS models (
             id TEXT PRIMARY KEY,
-            name TEXT NOT NUL
+            name TEXT NOT NULL,
+            summary TEXT NOT NULL,
+            size TEXT NOT NULL,
+            requires TEXT NOT NULL,
+            architecture TEXT NOT NULL,
+            released_at TEXT NOT NULL,
+            prompt
