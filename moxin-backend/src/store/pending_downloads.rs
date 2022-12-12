1ebf6246f6b229cae154c798575b9b97441b95a3
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2022-12-12 21:00:00 +0000

    minor mod

--- moxin-backend/src/store/pending_downloads.rs
+++ moxin-backend/src/store/pending_downloads.rs
@@ -1,4 +1,13 @@
 use rusqlite::Row;
 use std::{sync::Arc, vec};
 
-#[derive(Debug, Defa
+#[derive(Debug, Default, PartialEq, Clone)]
+pub enum PendingDownloadsStatus {
+    #[default]
+    Downloading,
+    Paused,
+    Error,
+}
+
+impl PendingDownloadsStatus {
+    pub fn to_st
