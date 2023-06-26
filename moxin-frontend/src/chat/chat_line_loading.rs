eec41db79196d1ea944a0fd4dbedf76763f234af
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-06-26 18:15:53 +0000

    add error handler

--- moxin-frontend/src/chat/chat_line_loading.rs
+++ moxin-frontend/src/chat/chat_line_loading.rs
@@ -3,4 +3,10 @@ use makepad_widgets::*;
 live_design! {
     import makepad_widgets::base::*;
     import makepad_widgets::theme_desktop_dark::*;
-   
+    import makepad_draw::shader::std::*;
+
+    import crate::shared::styles::*;
+    import crate::shared::widgets::*;
+    import crate::landing::model_card::ModelCard;
+
+    ANIMATION_SPEED 
