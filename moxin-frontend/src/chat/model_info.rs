d5a451eaee8e22ef425dfb7a111d47cc76dcb804
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-07-28 22:28:26 +0000

    Update ___jsl

--- moxin-frontend/src/chat/model_info.rs
+++ moxin-frontend/src/chat/model_info.rs
@@ -2,4 +2,13 @@ use makepad_widgets::*;
 
 live_design!{
     import makepad_widgets::base::*;
-    import makepad_widgets::t
+    import makepad_widgets::theme_desktop_dark::*;
+
+    import crate::shared::styles::*;
+
+    ModelAttributeTag = <RoundedView> {
+        width: Fit,
+        height: Fit,
+        padding: {top: 6, bottom: 6, left: 10, right: 10}
+
+       
