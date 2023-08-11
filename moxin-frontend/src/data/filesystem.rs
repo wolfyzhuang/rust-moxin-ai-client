1c4a2f26d17f56c050dd8940c564849b60050d74
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-08-11 09:39:36 +0000

    committed.

--- moxin-frontend/src/data/filesystem.rs
+++ moxin-frontend/src/data/filesystem.rs
@@ -2,4 +2,9 @@ use std::path::PathBuf;
 use std::{env, fs};
 
 // Note that .moxin will create a hidden folder in unix-like systems.
-// However in Windows the folder will be visible by defau
+// However in Windows the folder will be visible by default.
+pub const DEFAULT_DOWNLOADS_DIR: &str = ".moxin/model_downloads";
+pub const MOXIN_HOME_DIR: &str = ".moxin";
+
+pub fn setup_model_downloads_folder() -> String {
+    let home_dir = h
