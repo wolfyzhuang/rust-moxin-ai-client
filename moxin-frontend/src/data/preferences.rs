13e38b213283538684e42918dd8058f6d9f6d839
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-08-15 07:45:40 +0000

    Add cancel button

--- moxin-frontend/src/data/preferences.rs
+++ moxin-frontend/src/data/preferences.rs
@@ -12,4 +12,11 @@ const PREFERENCES_FILE: &str = "preferences.json";
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 pub struct Preferences {
-    pub current_chat_model: Option<File
+    pub current_chat_model: Option<FileID>,
+}
+
+impl Preferences {
+    pub fn load() -> Self {
+        match read_from_file() {
+            Ok(json) => {
+                let preferences: Preferences = serde_json::from_str(&json).u
