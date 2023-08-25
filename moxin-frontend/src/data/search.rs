7a55cd12af06585f349a300b40701758f04d1398
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-08-25 11:33:24 +0000

    HotBitmapGG

--- moxin-frontend/src/data/search.rs
+++ moxin-frontend/src/data/search.rs
@@ -6,4 +6,19 @@ use moxin_protocol::protocol::Command;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
 
-pub enum SearchAction 
+pub enum SearchAction {
+    Results(Vec<Model>),
+    Error,
+}
+
+#[derive(Clone)]
+pub enum SearchCommand {
+    Search(String),
+    LoadFeaturedModels,
+}
+
+#[derive(Default, Clone)]
+pub enum SearchState {
+    #[default]
+    Idle,
+   
