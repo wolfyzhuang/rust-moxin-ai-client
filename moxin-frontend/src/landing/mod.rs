4f67ed70b96ff9f0989f695fb1fc64bb97a8b492
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-09-28 10:54:49 +0000

    1.1.9

--- moxin-frontend/src/landing/mod.rs
+++ moxin-frontend/src/landing/mod.rs
@@ -5,4 +5,15 @@ pub mod model_card;
 pub mod model_files;
 pub mod model_files_item;
 pub mod model_files_list;
-pub mod model_files_
+pub mod model_files_tags;
+pub mod model_list;
+pub mod search_bar;
+pub mod search_loading;
+pub mod shared;
+pub mod sorting;
+
+use makepad_widgets::Cx;
+
+pub fn live_design(cx: &mut Cx) {
+    shared::live_design(cx);
+    model_files_tags::live_design(
