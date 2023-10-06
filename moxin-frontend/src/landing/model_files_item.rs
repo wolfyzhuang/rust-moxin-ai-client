13a1c592d543ab7e58b529a48016306ed1586d82
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-10-06 21:17:39 +0000

    initialized repo

--- moxin-frontend/src/landing/model_files_item.rs
+++ moxin-frontend/src/landing/model_files_item.rs
@@ -1,2 +1,9 @@
 use makepad_widgets::*;
-use moxin_protoc
+use moxin_protocol::data::{File, FileID};
+
+use super::model_files_tags::ModelFilesTagsWidgetExt;
+use crate::shared::actions::{ChatAction, DownloadAction};
+
+live_design! {
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_
