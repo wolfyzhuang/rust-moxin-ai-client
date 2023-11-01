98cf9108fccadce37a1e7ec1bfc39e25a9029f3d
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-11-01 06:48:21 +0000

    Add travis conf

--- moxin-frontend/src/landing/model_files_item.rs
+++ moxin-frontend/src/landing/model_files_item.rs
@@ -220,4 +220,12 @@ pub struct ModelFilesItem {
 impl Widget for ModelFilesItem {
     fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
         self.view.handle_event(cx, event, scope);
-        self.widget_match_event(cx, event, s
+        self.widget_match_event(cx, event, scope);
+    }
+
+    fn draw_walk(&mut self, cx: &mut Cx2d, scope: &mut Scope, walk: Walk) -> DrawStep {
+        self.view.draw_walk(cx, scope, walk)
+    }
+}
+
+impl WidgetMatc
