bfb13a63878133944b170ac5d815c36a31a84449
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-11-13 09:22:40 +0000

    Remove old cruft

--- moxin-frontend/src/landing/model_list.rs
+++ moxin-frontend/src/landing/model_list.rs
@@ -42,3 +42,127 @@ live_design! {
             show_bg: true,
             draw_bg: {
                 color: #FFFE,
+            }
+            search_loading = <SearchLoading> {}
+        }
+
+        search_error = <View> {
+            width: Fill,
+            height: Fill,
+            visible: false,
+            align: {x: 0.5, y: 0.5},
+
+            <Label> {
+                draw_text:{
+                    text_style: <REGULAR_FONT>{font_size: 13},
+                    color: #000
+                }
+                text: "Error fetching models. Please check your internet connection and try again."
+            }
+        }
+    }
+}
+
+#[derive(Live, LiveHook, Widget)]
+pub struct ModelList {
+    #[deref]
+    view: View,
+
+    #[rust]
+    loading_delay: Timer,
+}
+
+impl Widget for ModelList {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
+        self.view.handle_event(cx, event, scope);
+        self.widget_match_event(cx, event, scope);
+
+        if let Event::Signal = event {
+            self.loading_delay = cx.start_timeout(0.2);
+        }
+
+        if self.loading_delay.is_event(event).is_some() {
+            self.update_loading_and_error_message(cx, scope);
+        }
+    }
+
+    fn draw_walk(&mut self, cx: &mut Cx2d, scope: &mut Scope, walk: Walk) -> DrawStep {
+        let store = scope.data.get::<Store>().unwrap();
+        let models = &store.models;
+        let models_count = models.len();
+
+        while let Some(view_item) = self.view.draw_walk(cx, &mut Scope::empty(), walk).step() {
+            if let Some(mut list) = view_item.as_portal_list().borrow_mut() {
+                list.set_item_range(cx, 0, models_count);
+                while let Some(item_id) = list.next_visible_item(cx) {
+                    let item = list.item(cx, item_id, live_id!(Model)).unwrap();
+
+                    if item_id < models_count {
+                        let model_id = &models[item_id].id;
+                        let mut model_data =
+                            store.get_model_with_pending_downloads(model_id).unwrap();
+                        item.draw_all(cx, &mut Scope::with_data(&mut model_data));
+                    }
+                }
+            }
+        }
+
+        DrawStep::done()
+    }
+}
+
+#[derive(Clone, DefaultNone, Debug)]
+pub enum ModelListAction {
+    None,
+    ScrolledAtTop,
+    ScrolledNotAtTop,
+}
+
+const SCROLLING_AT_TOP_THRESHOLD: f64 = -30.0;
+
+impl WidgetMatchEvent for ModelList {
+    fn handle_actions(&mut self, cx: &mut Cx, actions: &Actions, scope: &mut Scope) {
+        let portal_list = self.portal_list(id!(list));
+
+        for action in actions.iter() {
+            match action.as_widget_action().cast() {
+                StoreAction::Search(_) | StoreAction::ResetSearch => {
+                    self.view(id!(search_error)).set_visible(false);
+                    self.view(id!(loading)).set_visible(true);
+                    self.search_loading(id!(search_loading)).animate(cx);
+                    portal_list.set_first_id_and_scroll(0, 0.0);
+
+                    self.redraw(cx);
+                }
+                _ => {}
+            }
+        }
+
+        if portal_list.scrolled(actions) {
+            let widget_uid = self.widget_uid();
+            if portal_list.first_id() == 0
+                && portal_list.scroll_position() > SCROLLING_AT_TOP_THRESHOLD
+            {
+                cx.widget_action(widget_uid, &scope.path, ModelListAction::ScrolledAtTop);
+            } else {
+                cx.widget_action(widget_uid, &scope.path, ModelListAction::ScrolledNotAtTop);
+            }
+        }
+    }
+}
+
+impl ModelList {
+    fn update_loading_and_error_message(&mut self, cx: &mut Cx, scope: &mut Scope) {
+        let store = scope.data.get::<Store>().unwrap();
+        let is_loading = store.search_is_loading();
+        self.view(id!(loading)).set_visible(is_loading);
+        if is_loading {
+            self.search_loading(id!(search_loading)).animate(cx);
+        } else {
+            self.search_loading(id!(search_loading)).stop_animation();
+        }
+
+        let is_errored = store.search_is_errored();
+        self.view(id!(search_error)).set_visible(is_errored);
+    }
+}
---
+++ moxin-frontend/src/landing/search_bar.rs
@@ -0,0 +1,2 @@
+use crate::data::store::{SortCriteria, StoreAction};
+use crat
