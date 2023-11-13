fc043cf4674d78704e7e46b5e1931a7fd126b1c0
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2023-11-13 14:58:45 +0000

    Bump up version

--- moxin-frontend/src/landing/search_bar.rs
+++ moxin-frontend/src/landing/search_bar.rs
@@ -1,2 +1,9 @@
 use crate::data::store::{SortCriteria, StoreAction};
-use crat
+use crate::landing::sorting::SortingWidgetExt;
+use makepad_widgets::*;
+
+live_design! {
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_desktop_dark::*;
+
+    import crate::
