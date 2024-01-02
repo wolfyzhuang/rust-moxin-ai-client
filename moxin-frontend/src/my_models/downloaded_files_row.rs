88a05a73dc912a8576517b6caf0fdf34605bf07c
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-01-02 15:35:37 +0000

    enhance README

--- moxin-frontend/src/my_models/downloaded_files_row.rs
+++ moxin-frontend/src/my_models/downloaded_files_row.rs
@@ -7,4 +7,10 @@ use crate::shared::utils::format_model_size;
 
 live_design! {
     import makepad_widgets::base::*;
-    import makepad_widget
+    import makepad_widgets::theme_desktop_dark::*;
+
+    import crate::shared::styles::*;
+    import crate::shared::widgets::*;
+
+    ICON_START_CHAT = dep("crate://self/resources/icons/start_chat.svg")
+    ICON_PLAY = dep("c
