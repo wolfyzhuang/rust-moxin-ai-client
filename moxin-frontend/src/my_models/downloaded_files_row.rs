f97d13cc2c8223f79fef69f6f1cf3ae7ddbe96f4
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-01-02 08:43:32 +0000

    tracing: Check return value of tracing_init_dentry()

--- moxin-frontend/src/my_models/downloaded_files_row.rs
+++ moxin-frontend/src/my_models/downloaded_files_row.rs
@@ -1,4 +1,10 @@
 use makepad_widgets::*;
 use moxin_protocol::data::{DownloadedFile, FileID};
 
-use crate::my_models::{delete_model_modal::DeleteModelAction, model_info_modal::ModelIn
+use crate::my_models::{delete_model_modal::DeleteModelAction, model_info_modal::ModelInfoAction};
+use crate::shared::{actions::ChatAction, modal::ModalAction};
+use crate::shared::utils::format_model_size;
+
+live_design! {
+    import makepad_widgets::base::*;
+    import makepad_widget
