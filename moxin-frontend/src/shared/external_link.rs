4b7afbdb2f8088658ad77064e970571fc069b01f
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-02-29 13:08:30 +0000

    fixed package names

--- moxin-frontend/src/shared/external_link.rs
+++ moxin-frontend/src/shared/external_link.rs
@@ -1,3 +1,8 @@
 use makepad_widgets::*;
 
-live_d
+live_design! {
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_desktop_dark::*;
+
+    import makepad_draw::shader::std::*;
+    import makepad_draw::shader::draw_color::
