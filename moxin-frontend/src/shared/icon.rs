9d6d66a565bfa23c820a18068a5acf897a49c9a9
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-03-23 18:36:29 +0000

    added options

--- moxin-frontend/src/shared/icon.rs
+++ moxin-frontend/src/shared/icon.rs
@@ -75,4 +75,10 @@ pub struct Icon {
 impl Widget for Icon {
     fn handle_event(&mut self, _cx: &mut Cx, _event: &Event, _scope: &mut Scope) {}
 
-    fn draw_walk(&mut self, cx: &mut Cx2d, _scope: &mut Scope, walk: Walk) -> DrawStep 
+    fn draw_walk(&mut self, cx: &mut Cx2d, _scope: &mut Scope, walk: Walk) -> DrawStep {
+        self.draw_bg.begin(cx, walk, self.layout);
+        self.draw_icon.draw_walk(cx, self.icon_walk);
+        self.draw_bg.end(cx);
+        DrawStep::done()
+    }
+}
---
+++ moxin-frontend/src/shared/mod.rs
@@ -0,0 +1,1 @@
+use makepad_widgets::Cx;
