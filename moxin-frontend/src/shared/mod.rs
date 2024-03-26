8291af5358d410dc714beece6bddb33b02c4977d
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-03-26 06:38:25 +0000

    UBI: fix a horrible memory deallocation bug

--- moxin-frontend/src/shared/mod.rs
+++ moxin-frontend/src/shared/mod.rs
@@ -1,1 +1,13 @@
 use makepad_widgets::Cx;
+
+pub mod actions;
+pub mod external_link;
+pub mod icon;
+pub mod modal;
+pub mod download_notification_popup;
+pub mod resource_imports;
+pub mod styles;
+pub mod utils;
+pub mod widgets;
+
+pub
