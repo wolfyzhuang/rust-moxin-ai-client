ddd1f3cdcda459a2aa56a8c292f1966c0c4c5bb7
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-03-31 19:07:55 +0000

    German dpkg translation update

--- moxin-frontend/src/shared/resource_imports.rs
+++ moxin-frontend/src/shared/resource_imports.rs
@@ -1,4 +1,6 @@
 use makepad_widgets::*;
 
 live_design! {
-    ICON_CLO
+    ICON_CLOSE = dep("crate://self/resources/icons/close.svg")
+    ICON_COPY = dep("crate://self/resources/icons/copy.svg")
+}
---
+++ moxin-frontend/src/shared/styles.rs
@@ -0,0 +1,5 @@
+use makepad_widgets::*;
+
+live_design! {
+    const MODEL_LINK_FONT_COLOR = #x155EEF
+    SIDEBAR_
