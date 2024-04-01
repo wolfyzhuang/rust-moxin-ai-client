497d15c669c916a89241548361cc9035b9d54dfb
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-04-01 22:02:03 +0000

    bump version 1.0.0

--- moxin-frontend/src/shared/styles.rs
+++ moxin-frontend/src/shared/styles.rs
@@ -2,4 +2,13 @@ use makepad_widgets::*;
 
 live_design! {
     const MODEL_LINK_FONT_COLOR = #x155EEF
-    SIDEBAR_
+    SIDEBAR_BG_COLOR = #F8F8F8
+
+    REGULAR_FONT = {
+        font_size: (12),
+        font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
+    }
+
+    BOLD_FONT = {
+        font_size: (12),
+   
