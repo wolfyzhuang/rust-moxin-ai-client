255534eb5db424ad2a9beceb80775041f98217eb
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-04-02 16:13:14 +0000

    Spelling Correction

--- moxin-frontend/src/shared/utils.rs
+++ moxin-frontend/src/shared/utils.rs
@@ -1,4 +1,10 @@
 use anyhow::Result;
 
 pub const BYTES_PER_MB: f64 = 1_048_576.0; // (1024^2)
-pub const HUGGING_FACE_BASE_URL: 
+pub const HUGGING_FACE_BASE_URL: &str = "https://huggingface.co";
+
+pub fn format_model_size(size: &str) -> Result<String> {
+    let size_mb = size.parse::<f64>()? / BYTES_PER_MB;
+
+    if size_mb >= 1024.0 {
+        Ok(format!("{:.2} GB", s
