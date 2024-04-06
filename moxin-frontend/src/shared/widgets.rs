24a0b39b67be4322c4c41f8565b498651c5c8722
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-04-06 09:18:54 +0000

    pedantry

--- moxin-frontend/src/shared/widgets.rs
+++ moxin-frontend/src/shared/widgets.rs
@@ -88,3 +88,162 @@ live_design! {
             fn pixel(self) -> vec4 {
                 let sdf = Sdf2d::viewport(self.pos * self.rect_size)
                 sdf.box(
+                    self.inset.x + self.border_width,
+                    self.inset.y + self.border_width,
+                    self.rect_size.x - (self.inset.x + self.inset.z + self.border_width * 2.0),
+                    self.rect_size.y - (self.inset.y + self.inset.w + self.border_width * 2.0),
+                    max(1.0, self.radius)
+                )
+                sdf.fill_keep(self.get_color())
+                if self.border_width > 0.0 {
+                    sdf.stroke(self.get_border_color(), self.border_width)
+                }
+                return sdf.result;
+            }
+        }
+
+        draw_text: {
+            color_unselected: (SIDEBAR_FONT_COLOR)
+            color_unselected_hover: (SIDEBAR_FONT_COLOR_HOVER)
+            color_selected: (SIDEBAR_FONT_COLOR_SELECTED)
+
+            fn get_color(self) -> vec4 {
+                return mix(
+                    mix(
+                        self.color_unselected,
+                        self.color_unselected_hover,
+                        self.hover
+                    ),
+                    self.color_selected,
+                    self.selected
+                )
+            }
+        }
+
+        draw_icon: {
+            instance color_unselected: (SIDEBAR_FONT_COLOR)
+            instance color_unselected_hover: (SIDEBAR_FONT_COLOR_HOVER)
+            instance color_selected: (SIDEBAR_FONT_COLOR_SELECTED)
+            fn get_color(self) -> vec4 {
+                return mix(
+                    mix(
+                        self.color_unselected,
+                        self.color_unselected_hover,
+                        self.hover
+                    ),
+                    self.color_selected,
+                    self.selected
+                )
+            }
+        }
+    }
+
+    // Customized button widget, based on the RoundedView shaders with some modifications
+    // which is a better fit with our application UI design
+    MoxinButton = <Button> {
+        draw_bg: {
+            instance color: #0000
+            instance color_hover: #fff
+            instance border_width: 1.0
+            instance border_color: #0000
+            instance border_color_hover: #fff
+            instance radius: 2.5
+
+            fn get_color(self) -> vec4 {
+                return mix(self.color, mix(self.color, self.color_hover, 0.2), self.hover)
+            }
+
+            fn get_border_color(self) -> vec4 {
+                return mix(self.border_color, mix(self.border_color, self.border_color_hover, 0.2), self.hover)
+            }
+
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size)
+                sdf.box(
+                    self.border_width,
+                    self.border_width,
+                    self.rect_size.x - (self.border_width * 2.0),
+                    self.rect_size.y - (self.border_width * 2.0),
+                    max(1.0, self.radius)
+                )
+                sdf.fill_keep(self.get_color())
+                if self.border_width > 0.0 {
+                    sdf.stroke(self.get_border_color(), self.border_width)
+                }
+                return sdf.result;
+            }
+        }
+
+        draw_icon: {
+            instance color: #fff
+            fn get_color(self) -> vec4 {
+                return mix(
+                    self.color,
+                    mix(self.color, #f, 0.2),
+                    self.hover
+                )
+            }
+        }
+        icon_walk: {width: 14, height: 14}
+
+        draw_text: {
+            text_style: <REGULAR_FONT>{font_size: 9},
+            fn get_color(self) -> vec4 {
+                return self.color;
+            }
+        }
+    }
+
+    // Customized text input
+    // Removes shadows, focus highlight and the dark theme colors
+    MoxinTextInput = <TextInput> {
+        draw_text: {
+            text_style:<REGULAR_FONT>{font_size: 12},
+            fn get_color(self) -> vec4 {
+                return #555
+            }
+        }
+
+        // TODO find a way to override colors
+        draw_cursor: {
+            instance focus: 0.0
+            uniform border_radius: 0.5
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                sdf.box(
+                    0.,
+                    0.,
+                    self.rect_size.x,
+                    self.rect_size.y,
+                    self.border_radius
+                )
+                sdf.fill(mix(#fff, #bbb, self.focus));
+                return sdf.result
+            }
+        }
+
+        // TODO find a way to override colors
+        draw_select: {
+            instance hover: 0.0
+            instance focus: 0.0
+            uniform border_radius: 2.0
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                sdf.box(
+                    0.,
+                    0.,
+                    self.rect_size.x,
+                    self.rect_size.y,
+                    self.border_radius
+                )
+                sdf.fill(mix(#eee, #ddd, self.focus)); // Pad color
+                return sdf.result
+            }
+        }
+
+        draw_bg: {
+            color: #fff
+            instance radius: 2.0
+            instance border_width: 0.0
+            instance border_color: #3
+            instance inset: vec4(0.0, 0.0, 0.0, 0.0)
