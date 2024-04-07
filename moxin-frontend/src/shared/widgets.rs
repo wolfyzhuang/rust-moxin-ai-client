06d90acca888ee9c0f7d659d97509555f1ad6f56
wolfyzhuang <166121680+wolfyzhuang@users.noreply.github.com>
2024-04-07 13:54:09 +0000

    project description

--- moxin-frontend/src/shared/widgets.rs
+++ moxin-frontend/src/shared/widgets.rs
@@ -247,3 +247,30 @@ live_design! {
             instance border_width: 0.0
             instance border_color: #3
             instance inset: vec4(0.0, 0.0, 0.0, 0.0)
+
+            fn get_color(self) -> vec4 {
+                return self.color
+            }
+
+            fn get_border_color(self) -> vec4 {
+                return self.border_color
+            }
+
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size)
+                sdf.box(
+                    self.inset.x + self.border_width,
+                    self.inset.y + self.border_width,
+                    self.rect_size.x - (self.inset.x + self.inset.z + self.border_width * 2.0),
+                    self.rect_size.y - (self.inset.y + self.inset.w + self.border_width * 2.0),
+                    max(1.0, self.radius)
+                )
+                sdf.fill_keep(self.get_color())
+                if self.border_width > 0.0 {
+                    sdf.stroke(self.get_border_color(), self.border_width)
+                }
+                return sdf.result;
+            }
+        }
+    }
+}
